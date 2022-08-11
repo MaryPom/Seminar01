984ac34a5f4a4677a47183682e8b041be5b99f47
Mariia <mashulkaegor@mail.ru>
2022-08-11 16:28:30 +0400

    add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 11. Найти третью цифру числа или сообщить, что её нет
+int n = new Random() .Next(1, 9999);
+Console.WriteLine(n);
+
+if (n > 99 && n < 999)
+{
+    Console.WriteLine(n % 10);
+}
+else if (n > 1000)
+{
+    Console.WriteLine(n % 100 / 10);
+}
+else
+{
+    Console.WriteLine( "В данном числе нет третьей цифры.");
+}
