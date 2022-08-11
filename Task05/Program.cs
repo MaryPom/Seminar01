f627c1cfcb759ad2e2355ac25d82c5ce3dc459eb
Mariia <mashulkaegor@mail.ru>
2022-08-11 16:16:08 +0400

    Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 5. Показать числа от -N до N
+Console.WriteLine("Введите значение N: ");
+int N = int.Parse (Console.ReadLine());
+int count = -N;
+
+while (count <= N)
+{
+    Console.WriteLine(count);
+    count = count + 1;
+}
