7f877d85c769fd6bb9d2fa783ce51b56634cf1b3
Mariia <mashulkaegor@mail.ru>
2022-08-11 16:26:45 +0400

    add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите число: ");
+double a = double.Parse(Console.ReadLine());
+double b = 2;
+double result = a / b;
+
+if (a % b == 0)
+{
+    Console.WriteLine("Заданное число кратно числу b!");
+}
+else
+{
+    Console.WriteLine("Заданное число не кратно числу b!" + " Остаток = " + result);
+}
