7d5f4baf6ba1878df92bd14ae820151b5467df12
Mariia <mashulkaegor@mail.ru>
2022-08-11 16:18:08 +0400

    add Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 6. Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите трёхзначное число: ");
+int n = int.Parse (Console.ReadLine());
+
+if (n >= 100 && n < 1000)
+{
+    int last = n % 10;
+    int first = n / 100;
+    int result = last;
+    Console.WriteLine("Последняя цифра трёхзначного числа: " + result);
+}
+else
+{
+    Console.WriteLine("Это не трёхзначное число");
+}
