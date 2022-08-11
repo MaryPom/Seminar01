560ba87dd923d5e990beb77996279c4d3edcfca6
Mariia <mashulkaegor@mail.ru>
2022-08-11 16:24:32 +0400

    add Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 9. Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трёхзначное число: ");
+int n = int.Parse (Console.ReadLine());
+
+if (n >= 100 && n < 1000)
+{
+    int last = n % 10;
+    int first = n / 100;
+    int second = n / 10 % 10;
+    int result = first * 10 + last;
+    Console.WriteLine("Число без второй цифры: " + result);
+}
+else
+{
+    Console.WriteLine("Это не трёхзначное число");
+}
